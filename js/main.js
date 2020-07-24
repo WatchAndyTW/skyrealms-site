dfc73fa00d82df4291ca0fd6e7a93e39735090c0
WatchAndyTW <51250139+WatchAndyTW@users.noreply.github.com>
2020-07-24 17:27:19 +0800

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,21 @@
+let t;
+$(document).ready(()=>{
+    t = $(".download").html();
+})
+$(document).on("click",".download",()=>{
+    let copy = document.createElement("textarea");
+    copy.style.position = "absolute";
+    copy.style.left = "-99999px";
+    copy.style.top = "0";
+    copy.setAttribute("id", "ta");
+    document.body.appendChild(copy);
+    copy.textContent = "skyrealms.mcbe.tw";
+    copy.select();
+    document.execCommand("copy");
+    $(".download").html("<span class='extrapad'>IP已複製!</span>");
+    setTimeout(function(){
+        $(".download").html(t);
+        var copy = document.getElementById("ta");
+        copy.parentNode.removeChild(copy);
+    },800);
+});
